6873fe3ef332fe9d957f88fc9617315d92fffd6b
lionel TSIMI <l.tsimi.ext@oodrive.com>
2022-02-20 23:35:29 +0100

    update roundcube database creation script

--- roles/webmailserver/files/createdb.sql
+++ roles/webmailserver/files/createdb.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE IF NOT EXISTS roundcubemail CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON roundcubemail.* TO roundcube@'%' IDENTIFIED BY 'roundcubepassword';
+CREATE DATABASE IF NOT EXISTS ldaproundcubemail CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL PRIVILEGES ON ldaproundcubemail.* TO roundcube@'%' IDENTIFIED BY 'roundcubepassword';
 FLUSH PRIVILEGES;
